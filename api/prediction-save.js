7d4a9b52e0e3a70bfa4655b70adfd8a286d70155
blithe886420-droid <blithe886420@gmail.com>
2026-03-15 16:47:48 +0800

    Update prediction-save.js

--- api/prediction-save.js
+++ api/prediction-save.js
@@ -860,7 +860,7 @@ export default async function handler(req, res) {
       groups,
       prediction_strategy_map_result: predictionStrategyMapResult,
       bet_group_count: BET_GROUP_COUNT,
-      cost_per_group_per_period: COST_PER_GROUP_COUNT,
+      cost_per_group_per_period: COST_PER_GROUP_PER_PERIOD,
       estimated_total_cost: BET_GROUP_COUNT * targetPeriods * COST_PER_GROUP_PER_PERIOD
     });
   } catch (err) {
